b4e67ddb28958ede36ab706ed69731a64aff0042
Walsssss <108856194+Walsssss@users.noreply.github.com>
2026-01-14 19:41:47 +0800

    Add files via upload

---
+++ src/main/java/walson/me/walsondev/warningsplugin/WarningsPlugin.java
@@ -0,0 +1,17 @@
+package walson.me.walsondev.warningsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WarningsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
